0b0c52c962603bf74f1f50388bbeef10d7559de9
emilh91 <emilh_91@yahoo.com>
2014-03-13 01:58:26 -0400

    Adds classes Symbol, SymbolAttribute, and SymbolTable

---
+++ CompilerCore/SymbolAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace CompilerCore
+{
+    internal class SymbolAttribute
+    {
+        internal TokenType TokenType { get; set; } // TODO Consider moving this up to Symbol ...
+
+        internal SemanticType SemanticType { get; set; }
+
+        internal DataType DataType { get; set; }
+
+        internal int ParentAttributeIndex { get; private set; }
+
+        internal SymbolAttribute(int parentIdx)
+        {
+            TokenType  = TokenType.Unknown;
+            SemanticType = SemanticType.Unknown;
+            DataType = DataType.Unknown;
+            ParentAttributeIndex = parentIdx;
+        }
+    }
+}
---
+++ CompilerCore/SymbolTable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CompilerCore
+{
+    internal class SymbolTable : ISymbolTable
+    {
+        private Dictionary<string, Symbol> Symbols { get; set; }
+
+        internal SymbolTable()
+        {
+            Symbols = new Dictionary<string, Symbol>();
+            LoadKeywords();
+            LoadOperators();
+        }
+
+        public bool ContainsString(string lexeme)
+        {
+            return Symbols.ContainsKey(lexeme);
+        }
+
+        internal TokenType GetTokenTypeFor(string lexeme)
+        {
+            return GetOrCreateSymbolFor(lexeme).CurrentAttribute.TokenType;
+        }
+
+        private Symbol GetOrCreateSymbolFor(string lexeme)
+        {
+            if (ContainsString(lexeme))
+            {
+                return Symbols[lexeme];
+            }
+
+            var symbol = new Symbol(lexeme);
+            Symbols[lexeme] = symbol;
+            return symbol;
+        }
+
+        private void LoadKeywords()
+        {
+            foreach (var symbol in Utils.GetKeywords().Select(GetOrCreateSymbolFor))
+            {
+                symbol.CurrentAttribute.SemanticType = SemanticType.Keyword;
+            }
+        }
+
+        private void LoadOperators()
+        {
+            foreach (var symbol in Utils.GetOperators().Select(GetOrCreateSymbolFor))
+            {
+                symbol.CurrentAttribute.SemanticType = SemanticType.Operator;
+            }
+        }
+    }
+}
