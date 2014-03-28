9616ad89815be927511e1fb1579044b636a56416
emilh91 <emilh_91@yahoo.com>
2014-03-27 22:57:10 -0400

    Adds another implementation of ISymbolTable, called SymbolTableTreeImpl that can be swapped with SymbolTableLinkedImpl in CompilerFrontend class
    Also adds Scope class which can be thought of as a TreeNode<Scope>

---
+++ CompilerCore/Scope.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace CompilerCore
+{
+    internal class Scope
+    {
+        internal Scope ParentScope { get; private set; }
+
+        private List<Scope> ChildScopes { get; set; }
+
+        private Dictionary<string, ISymbol> SymbolMap { get; set; }
+
+        internal Scope(Scope parentScope = null)
+        {
+            ParentScope = parentScope;
+            ChildScopes = new List<Scope>();
+            SymbolMap = new Dictionary<string, ISymbol>();
+        }
+
+        internal Scope OpenNewScope()
+        {
+            var scope = new Scope(this);
+            ChildScopes.Add(scope);
+            return scope;
+        }
+
+        internal IEnumerable<Scope> Ancestry()
+        {
+            var curr = this;
+
+            do
+            {
+                // The "yield" keyword marks this method as an iterator block
+                yield return curr;
+                curr = curr.ParentScope;
+            } while (curr != null);
+        }
+
+        internal ISymbol GetSymbolFor(string lexeme)
+        {
+            return SymbolMap[lexeme];
+        }
+
+        internal bool ContainsString(string lexeme)
+        {
+            return SymbolMap.ContainsKey(lexeme);
+        }
+
+        internal ISymbol InstallSymbol(string lexeme)
+        {
+            // TODO Have different logic if the symbol exists?
+            var symbol = new SymbolImpl(lexeme);
+            SymbolMap[lexeme] = symbol;
+            return symbol;
+        }
+    }
+}
