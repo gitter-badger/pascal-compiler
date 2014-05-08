f0487a363cd47594d79028ef149698632f2d68af
emilh91 <emilh_91@yahoo.com>
2014-05-07 23:48:38 -0400

    Adds ICompilerFrontend interface

---
+++ CompilerCore/Interfaces/ICompilerFrontend.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompilerCore.Interfaces
+{
+    public interface ICompilerFrontend
+    {
+        void Go(string outputPath = null);
+    }
+}
